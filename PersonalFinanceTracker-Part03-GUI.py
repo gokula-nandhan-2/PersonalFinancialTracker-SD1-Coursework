1bb4c6397ac695fcb7fc4cc15c9775242f232f21
Gokula Nandhan <gokula.nandhan02@gmail.com>
2025-08-08 08:37:05 +0530

    done the button and search bar

--- PersonalFinanceTracker-Part03-GUI.py
+++ PersonalFinanceTracker-Part03-GUI.py
@@ -9,10 +9,10 @@ class FinanceTrackerGUI:
         self.root.iconbitmap("icon.ico")
 
         height, width = 600, 800
-        dispaly_width = self.root.winfo_screenwidth()
+        display_width = self.root.winfo_screenwidth()
         display_height = self.root.winfo_screenheight()
 
-        left = int((dispaly_width / 2) - (width / 2))
+        left = int((display_width / 2) - (width / 2))
         top = int((display_height / 2) - (height / 2))
         self.root.geometry(f'{width}x{height}+{left}+{top}')
     
@@ -30,6 +30,13 @@ class FinanceTrackerGUI:
         
 
         # Search bar and button
+        self.search_icon = tk.PhotoImage(file="search.ico")
+        searchFrame = ttk.Frame(self.root)
+        searchFrame.pack(pady=20)
+        searchEntry = ttk.Entry(searchFrame, width=40)
+        searchEntry.pack(side=tk.LEFT, padx=(0, 10))
+        searchButton = ttk.Button(searchFrame, image=self.search_icon, command=self.search_transactions)
+        searchButton.pack(side=tk.LEFT)
 
         pass
         
